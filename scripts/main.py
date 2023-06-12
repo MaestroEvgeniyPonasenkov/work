67e06a7054576fb7ce604fa6f0a0fee0b0fdce9c
Ivan <Riadnov.ivan@yandex.ru>
2023-06-12 16:28:10 +0300

    deleted slice

--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,7 @@ import tkinter as tk
 from tkinter import ttk, Entry, colorchooser
 from tkinter.ttk import Treeview, Spinbox
 
-os.chdir("\\".join(os.getcwd().split("\\")[:-1]))
+os.chdir("\\".join(os.getcwd().split("\\")))
 sys.path.append("\\".join(os.getcwd().split("\\")))
 sys.path.append("\\".join(os.getcwd().split("\\")) + '\\library')
 from library.read_ini import read_ini_file, update_ini_value
