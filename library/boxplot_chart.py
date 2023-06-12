f3485a19f966ae9f4092b57dff907943741e0d29
Ivan <Riadnov.ivan@yandex.ru>
2023-06-12 16:23:07 +0300

    added dev notes

--- library/boxplot_chart.py
+++ library/boxplot_chart.py
@@ -14,7 +14,6 @@ def report_price_by_category(data: pd.DataFrame) -> None:
     prices_by_category = [category[1]['Price'].tolist()
                           for category in categories]
     plt.boxplot(prices_by_category)
-    print([category[0] for category in categories])
     plt.xticks(range(1, len(categories) + 1),
                [category[0] for category in categories], rotation=60, ha='right')
     plt.title(
